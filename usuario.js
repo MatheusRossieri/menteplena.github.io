544c5b34ad4f17e4beda0b01ad084437696d5147
Ahalekin <118134379+Ahalekin@users.noreply.github.com>
2024-06-21 20:25:11 -0300

    Add files via upload

--- usuario.js
+++ usuario.js
@@ -9,10 +9,19 @@ class Usuario {
 function cadastrarUsuario(nome, email, senha) {
     const usuarios = JSON.parse(localStorage.getItem('usuarios') || '[]');
     let i = 0;
-    for(i; usuarios[i].email != email; i++);
-    if(usuarios[i].email != email){
-        const novoUsuario = new Usuario(nome, email, senha); // Cria um novo objeto Usuario
-        usuarios.push(novoUsuario);
-        localStorage.setItem('usuarios', JSON.stringify(usuarios));
+    if(usuarios[i] != null){
+        for(i; usuarios[i].email != email; i++);
+        if(usuarios[i].email != email){
+            memoriaTemp(nome, email, senha);
+        }
     }
+    else{
+        memoriaTemp(nome, email, senha);
+    }
+}
+
+function memoriaTemp(nome, email, senha){
+    const novoUsuario = new Usuario(nome, email, senha); // Cria um novo objeto Usuario
+    usuarios.push(novoUsuario);
+    localStorage.setItem('usuarios', JSON.stringify(usuarios));
 }
